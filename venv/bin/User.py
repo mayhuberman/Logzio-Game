05bf72635aff0b5c2f63595b58173432cd15e9cd
mayhuberman <may.huberman@logz.io>
2018-12-30 16:36:46 +0200

    first commit

---
+++ venv/bin/User.py
@@ -0,0 +1,6 @@
+
+class User(object):
+    def __init__(self, name, highestScore, currentScore):
+        self.name = name
+        self.highestScore = highestScore
+        self.currentScore = currentScore
---
+++ venv/bin/questions.py
@@ -0,0 +1,8 @@
+from Question import Question
+
+
+questions = [
+    Question("What is the first component that logs that are shipped to Logz.io get to", 2, ["Listener", "HAProxy", "Nginx", "Services"]),
+    Question("Which component writes to Kafka", 3, ["Log engine", "Tasks", "Listener", "Ajduster"]),
+    Question("Who reads from Kafka", 1, ["Log engine, archiver, tailer, atlas", "Log engine, atlas, Apollo", "Scanner, archiver, services", "Apollo, log, engine, services"])
+]
